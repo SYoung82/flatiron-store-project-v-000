e2ad1c5d10524e42c29d16c001bee263b14e86aa
Scott Young <young.scottw@gmail.com>
2017-02-21 16:24:52 -0600

    Added Cart model functions #total and #add_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,19 @@ class Cart < ActiveRecord::Base
   has_many :line_items
   has_many :items, through: :line_items
   belongs_to :user
+
+  def total
+    self.items.sum(:price)
+  end
+
+  def add_item(item_id)
+    line_item = self.line_items.find_by(item_id: item_id)
+    if line_item
+      line_item.quantity += 1
+    else
+      line_item = self.line_items.build(item_id: item_id)
+    end
+    line_item
+  end
+
 end
